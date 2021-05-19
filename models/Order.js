46bdfccb0207ab7f45312f346a8af5255ac33688
EsraaFarhat <esraafarhat97@gmail.com>
2021-05-19 15:26:18 +0200

    Add owner in Order model

--- models/Order.js
+++ models/Order.js
@@ -14,6 +14,12 @@ const orderSchema = new mongoose.Schema({
       ref: "Product",
     },
   ],
+  owner: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  ],
 });
 
 const Order = mongoose.model("Order", orderSchema);
